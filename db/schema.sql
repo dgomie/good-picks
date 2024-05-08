3a5c32a6aeefd80f9cf83f7e4f5510e983434d2d
Danny <danny.f.gomez@gmail.com>
2024-05-07 20:40:00 -0400

    changed route names to start server

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,23 @@ CREATE DATABASE `goodPicks_db`;
 
 USE `goodPicks_db`;
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `favorite` varchar(50) ALLOW NULL,
-  PRIMARY KEY (`id`)
-) 
 
-CREATE TABLE `music` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(255) NOT NULL,
-    `artist` varchar(255) NOT NULL,
-    `genre` varchar(255) NOT NULL,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-    ) 
+-- clean up once models/seeds are working
+-- CREATE TABLE `users` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT,
+--   `username` varchar(255) NOT NULL,
+--   `password` varchar(255) NOT NULL,
+--   `email` varchar(255) NOT NULL,
+--   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   `favorite` varchar(50) ALLOW NULL,
+--   PRIMARY KEY (`id`)
+-- ) 
+
+-- CREATE TABLE `music` (
+--     `id` int(11) NOT NULL AUTO_INCREMENT,
+--     `title` varchar(255) NOT NULL,
+--     `artist` varchar(255) NOT NULL,
+--     `genre` varchar(255) NOT NULL,
+--     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     PRIMARY KEY (`id`)
+--     ) 
