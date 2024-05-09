820f06bd134b47498355b33d6dc5cbb93153afa9
Danny <danny.f.gomez@gmail.com>
2024-05-08 23:15:10 -0400

    Add music route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,5 +3,6 @@ const userRoutes = require('./user-routes');
 const musicRoutes = require('./music-routes');
 
 router.use('/users', userRoutes);
+router.use('/music', musicRoutes)
 
 module.exports = router;
