2a1f08ca45e37799fb0069d895f5f9840d159f17
Danny <danny.f.gomez@gmail.com>
2024-05-10 21:43:27 -0400

    heroku update

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 // changed the import statement to match the actual file name
-const Music = require('./Music');
+const Music = require('./music');
 // I'm iffy about this one because it wasn't working with the actual file name when it was uppercase
 const Rating = require('./rating'); // Fix: Change the import statement to match the actual file name
 
