6811cef4cc27f9e48cc4cadb2dd24b4c652cfaf3
Danny <danny.f.gomez@gmail.com>
2024-05-13 19:31:51 -0400

    add comments to songcard.handlebars

--- public/js/register.js
+++ public/js/register.js
@@ -4,11 +4,9 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#username-register').value.trim();
     const email = document.querySelector('#email-register').value.trim();
     const password = document.querySelector('#password-register').value.trim();
-    const passwordConfirm = document.querySelector('#password-confirm').value.trim();
 
     console.log(username, email, password)
-
-    if (password === passwordConfirm) {
+  
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
@@ -21,10 +19,8 @@ const signupFormHandler = async (event) => {
       } else {
         alert('Failed to sign up.');
       }
-    } else {
-      alert('passwords must match');
     }
-  }};
+  };
 
   document
   .querySelector('#register-form')
