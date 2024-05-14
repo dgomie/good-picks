7306e00782e425073bdf04c44ea4d843c53221f5
TheQuestioningCake <perrywayne39@gmail.com>
2024-05-14 19:12:18 -0400

    fixing mistake and readding helper.js

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+const Handlebars = require('handlebars');
+Handlebars.registerHelper('generateStars', function(rating) {
+    let stars = '';
+    for (let i = 0; i < rating; i++) {
+        stars += '★';
+    }
+    for (let i=0; stars.length <= 4; i++) {
+        stars += '☆'
+    }
+    return new Handlebars.SafeString(stars);
+});
+Handlebars.registerHelper('renderAlbumImages', function(albumImages) {
+    const albumImagesHtml = albumImages.map(imageUrl => {
+      return `<img src="${imageUrl}" alt="Album Image">`;
+    }).join('');
+    return new Handlebars.SafeString(albumImagesHtml);
+  });
