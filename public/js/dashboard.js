be126ce925c11aa0a7c25b59dd1929a79b262d44
Danny <danny.f.gomez@gmail.com>
2024-05-15 01:20:45 -0400

    Receive inputs from create post modal

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,11 @@
+// const { User, Music, Rating } = require('../../models');
+document.addEventListener('DOMContentLoaded', (event) => {
 const songName = document.getElementById('name').value.trim();
 const artistName = document.getElementById('artist-pick').value.trim();
 const albumName = document.getElementById('album-pick').value.trim();
 const genre = document.getElementById('category').value.trim();
 let rating = 1
-// Get all the stars
+
 let stars = document.getElementsByName('rating');
 
 // Add an event listener to each star
@@ -13,3 +15,38 @@ for(let i = 0; i < stars.length; i++) {
         rating = parseInt(this.value, 10)
     });
 }
+
+window.addEventListener('submit', function(event) {
+    if (event.target.matches('#formID')) {
+        event.preventDefault();
+            console.log("song name", songName);
+            console.log("artist", artistName);
+            console.log("album name", albumName);
+            console.log("genre", genre);
+            clearInputs()
+    // create artist name and pull img if doesn't exist in db
+    // create music item with song name/album name/genre/and artist_id
+    // create rating with rating, user_id, music_id,
+    }
+}, true);
+
+
+const clearInputs = () =>{
+    document.getElementById('name').value = '';
+    document.getElementById('artist-pick').value = '';
+    document.getElementById('album-pick').value = '';
+    document.getElementById('category').value = '';
+    const radios = document.querySelectorAll('input[type="radio"][name="rating"]');
+
+    // Loop over the radio buttons and set their checked property to false
+    radios.forEach(radio => {
+        radio.checked = false;
+    });
+    
+}
+
+
+
+
+
+});
